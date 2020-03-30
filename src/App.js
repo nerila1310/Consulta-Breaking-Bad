7cc770a75d19e0ff2673558d1f11c9d460cb9588
Neri-Laredo <nerila1310@gmail.com>
2020-03-29 23:04:02 -0600

    Agregamos el useEffect para cargar una frase al inicio

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import styled from '@emotion/styled';
 import Frase from './components/Frase';
 
@@ -19,6 +19,13 @@ const Boton = styled.button`
     padding: 1rem 3rem;
     font-size: 2rem;
     border: 2px solid black;
+    transition: background-size .8s ease;
+
+    :hover {
+        cursor: pointer;
+        background-size: 400px;
+
+    }
 `;
 
 function App() {
@@ -31,6 +38,11 @@ function App() {
         const frase = await api.json();
         guardarFrase(frase[0]);
     }
+
+    //cargar una frase
+    useEffect(() => {
+        consultarAPI()
+    }, [])
 
     return (
         <Contenedor>
